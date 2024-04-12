0f2772a478f15e3555d4f695440b6b44bf27b2e9
Mathias Cody <codydematth@gmail.com>
2024-04-12 05:03:10 +0100

    Revoke changes

--- website/settings.py
+++ website/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
    'default': dj_database_url.config(       
     # Replace this value with your local database's connection string.        
-    default='postgresql://postgres:postgres@localhost:5432/codydematthApi',       
+    default='postgresql://postgres:postgres@localhost:5432/mysite',       
     conn_max_age=600    )
 }
 
