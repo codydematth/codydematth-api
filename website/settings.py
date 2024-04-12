59158c71fc79092bf7761f7343f37a24e5fb817b
Mathias Cody <codydematth@gmail.com>
2024-04-12 04:16:20 +0100

    Onrender Deploy

--- website/settings.py
+++ website/settings.py
@@ -1,3 +1,5 @@
+# Import dj-database-url at the beginning of the file.
+import dj_database_url
 """
 Django settings for website project.
 
@@ -77,10 +79,10 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.config(       
+    # Replace this value with your local database's connection string.        
+    default='postgresql://postgres:postgres@localhost:5432/mysite',       
+    conn_max_age=600    )
 }
 
 
